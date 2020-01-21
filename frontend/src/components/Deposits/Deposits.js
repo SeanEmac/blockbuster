3bbd66a04b038a92a690d86ee2d5658adc6c995c
seanemac <S.mccann11@nuigalway.ie>
2020-01-21 20:01:59 +0000

    Display inputs and outputs for a transaction

--- frontend/src/components/Deposits/Deposits.js
+++ frontend/src/components/Deposits/Deposits.js
@@ -1,45 +1,95 @@
 import React, { useState, useEffect } from "react";
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Title from '../Title/Title';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   depositContext: {
     flex: 1,
   },
-});
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
 
 const Deposits = (props)  => {
   const classes = useStyles()
   const [hasError, setErrors] = useState(false);
-  const [transactionObj, setTransactionObj] = useState({
-    hash: '',
-    inputsAddr: '',
-    inputAmt: '',
-    outputsAddr: '',
-    outputsAmt: ''
-  });
+  const [transactionObj, setTransactionObj] = useState({ data: null });
+  const [inputOpen, setInputOpen] = React.useState(false);
+  const [outputOpen, setOutputOpen] = React.useState(false);
+
+  const handleInputClick = () => {
+    setInputOpen(!inputOpen);
+  };
+
+  const handleOutputClick = () => {
+    setOutputOpen(!outputOpen);
+  };
   
   async function fetchData(trans) {
-    if(trans != "") {
+    if (trans !== "") {
       const res = await fetch('https://blockchain.info/rawtx/' + trans + '?&cors=true')
       res
         .json()
-        .then(res => setTransactionObj({
-          hash: res.hash,
-          inputsAddr: res.inputs[0].prev_out.addr,
-          inputAmt: res.inputs[0].prev_out.value/100000000,
-          outputsAddr: res.out[0].addr,
-          outputsAmt: res.out[0].value/100000000,
-        }))
+        .then(res => setTransactionObj({ data: res }))
         .catch(err => setErrors(err));
     }
   }
+  
+  const createInputs = (()=> {
+    let inputs = transactionObj.data.inputs
+    let collapse = []
+    
+    inputs.forEach(input => {
+      let buttonText = input.prev_out.addr + ' : ' + input.prev_out.value / 100000000 + ' BTC'
+      collapse.push(
+        <Collapse in={inputOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary={buttonText} />
+            </ListItem>
+          </List>
+        </Collapse>
+      );   
+    });
+
+    return collapse
+  });
+
+  const createOutputs = (()=> {
+    let outputs = transactionObj.data.out
+    let collapse = []
+    
+    outputs.forEach(output => {
+      let buttonText = output.addr + ' : ' + output.value / 100000000 + ' BTC'
+      collapse.push(
+        <Collapse in={outputOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary={buttonText} />
+            </ListItem>
+          </List>
+        </Collapse>
+      );
+    });
+
+    return collapse
+  });
   
   useEffect(() => {
     fetchData(props.transID);
@@ -48,13 +98,28 @@ const Deposits = (props)  => {
   return (
     <React.Fragment>
      <Title>Transaction:</Title>
-     <div>Hash: {transactionObj.hash}</div>
 
-     <div>Input: {transactionObj.inputsAddr}</div>
-     <div>In Amount: {transactionObj.inputAmt}</div>
+      {/* This will only render when we have gotten a response */}
+      { transactionObj && transactionObj.data &&
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          className={classes.root}
+        >
+          <ListItem button onClick={handleInputClick}>
+            <ListItemText primary="Inputs" />
+            {inputOpen ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          {createInputs()}
 
-     <div>Output: {transactionObj.outputsAddr}</div>
-     <div>Out Amount: {transactionObj.outputsAmt}</div>
+          <ListItem button onClick={handleOutputClick}>
+            <ListItemText primary="Outputs" />
+            {outputOpen ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          {createOutputs()}
+
+        </List>
+      }
    </React.Fragment>
   )
 }
