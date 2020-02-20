968dd520d9dfd47b8ce5b688113672dd17f0d366
seanemac <S.mccann11@nuigalway.ie>
2020-02-20 18:44:02 +0000

    Lots of work extending out one step

--- backend/server.py
+++ backend/server.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 import requests
+import time
 
 app = Flask(__name__)
 cors = CORS(app)
 
 
-def process_response(response):
+def process_transaction(response):
   res = {}
   res['id'] = response['hash']
   res['timestamp'] = response['time']
@@ -16,15 +17,26 @@ def process_response(response):
 
   for inp in response['inputs']:
     input = {}
-    input['input_key'] = inp['prev_out']['addr']
     input['satoshis'] = inp['prev_out']['value']
+    input['spent'] = inp['prev_out']['spent']
+    if ('addr' in inp['prev_out']):
+      input['address'] = inp['prev_out']['addr']
+    else:
+      input['address'] = 'Invalid Address'
+
     res['inputs'].append(input)
 
   for out in response['out']:
     output = {}
-    output['output_key'] = out['addr']
+    output['spent'] = out['spent']
     output['satoshis'] = out['value']
+    if ('addr' in out):
+      output['address'] = out['addr']
+    else: 
+      output['address'] = 'Invalid Address'
+
     res['outputs'].append(output)
+
 
   res['totalIn'] = sum(x['satoshis'] for x in res['inputs'])
   res['totalOut'] = sum(x['satoshis'] for x in res['outputs'])
@@ -32,126 +44,133 @@ def process_response(response):
   return res
 
 
-def make_request(transaction_id):
-  # url = "https://blockchain.info/rawtx/" + transaction_id + "?&cors=true"
-  # response = requests.get(url).json()
-  # return process_response(response)
-  t2 = {
-    'id': '9fa4e0e33aba41623bc3618827d2a6495e6828ce04f26c97771a1369210e8201',
-    'timestamp': 1322135154000,
-    'totalIn': 4070000000,
-    'totalOut': 4070000000,
-    'fees': 0,
-    'inputs': [
-      {
-        'address': '1ExY9836X5RB5GA28KCtegh5iVenzp4pch',
-        'satoshis': 4070000000,
-        'parent_transaction': 'leave out'
-      }
-    ],
-    'outputs': [
-      {
-        'address': '1FwYmGEjXhMtxpWDpUXwLx7ndLNfFQncKq',
-        'satoshis': 100000000,
-        'parent_transaction': 'leave out'
-      },
-      {
-        'address': '13 more',
-        'satoshis': 3970000000,
-        'parent_transaction': '13 more'
-      }
-    ]
-  }
+def get_root_trans(transaction_id):
+  url = "https://blockchain.info/rawtx/" + transaction_id + "?&cors=true"
+  response = requests.get(url).json()
+  return process_transaction(response)
 
 
-  t3 = {
-    'id': 'df0f8a4f0988de2875705a79ec826c8b9f8b08c9ffa4e5b4a5ea1b7bf956306c',
-    'timestamp': 1322135154000,
-    'totalIn': 10100000000,
-    'totalOut': 10100000000,
-    'fees': 99999,
-    'inputs': [
-      {
-        'address': '14pDqB95GWLWCjFxM4t96H2kXH7QMKSsgG',
-        'satoshis': 98000000,
-        'parent_transaction': 'leave out'
-      },
-      {
-        'address': 'there are 3 others',
-        'satoshis': 10002000000,
-        'parent_transaction': 'leave out'
-      }
-    ],
-    'outputs': [
-      {
-        'address': 'there are 2 more',
-        'satoshis': 10100000000,
-        'parent_transaction': 'leave out'
-      }
-    ]
-  }
-
-  t4 = {
-    'id': '0c45329983279acf2f9e9c7976774ef11bdb2646d934836c53e6679281e09ac8',
-    'timestamp': 1322135154000,
-    'totalIn': 400000000,
-    'totalOut': 400000000,
-    'fees': 0,
-    'inputs': [
-      {
-        'address': '13AMPUTTwryLGX3nrMvumaerSqNXkL3gEV',
-        'satoshis': 2000000,
-        'parent_transaction': 'leave out'
-      },
-      {
-        'address': 'lots of others 111',
-        'satoshis': 398000000,
-        'parent_transaction': 'leave out'
-      }
-    ],
-    'outputs': [
-      {
-        'address': 'there are 2 other',
-        'satoshis': 400000000,
-        'parent_transaction': 'leave out'
-      }
-    ]
-  }
+def get_address(address):
+  # need to collect all of the transactions of the address using limits and offset
+  url = "https://blockchain.info/rawaddr/" + address + "?&cors=true"
+  return requests.get(url).json()
 
 
-  root = {
-    'id': 'b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da',
-    'timestamp': 1322135154000,
-    'totalIn': 100000000,
-    'totalOut': 100000000,
-    'fees': 0,
-    'inputs': [
-      {
-        'address': '1FwYmGEjXhMtxpWDpUXwLx7ndLNfFQncKq',
-        'satoshis': 100000000,
-        'parent': t2
-      }
-    ],
-    'outputs': [
-      {
-        'address': '13AMPUTTwryLGX3nrMvumaerSqNXkL3gEV',
-        'satoshis': 2000000,
-        'child': t4
-      },
-      {
-        'address': '14pDqB95GWLWCjFxM4t96H2kXH7QMKSsgG',
-        'satoshis': 98000000,
-        'child': t3
-      }
-    ]
-  }
-  return root
+def filter_address(transactions, isInput):
+  # Filters out transactions that are a different base or not spent
+  filtered_transactions = []
+  for transaction in transactions:
+    all_good = True
+
+    if isInput:
+      for input in transaction['inputs']:
+        if not 'addr' in input['prev_out']:
+          all_good = False
+    else:
+      for output in transaction['out']:
+        if not 'addr' in output:
+          all_good = False
+    
+    if all_good:
+      filtered_transactions.append(transaction)
+
+  return filtered_transactions
+
+
+def next_spending_transaction(output):
+  # Tries to match up where the transaction was spent
+  addr = output['address']
+  value = output['satoshis']
+  address = get_address(addr)
+  valid_transactions = filter_address(address["txs"], True)
+
+  spending_transaction = [tx for tx in valid_transactions # A list of transactions where:
+    if ( addr in [y["prev_out"]["addr"] for y in tx["inputs"]] # The address is an input
+    and value in [y["prev_out"]["value"] for y in tx["inputs"]])] # And the value matches
+
+  log_results(spending_transaction)
+
+  return spending_transaction[0]
+
+
+def log_results(spending_transaction):
+  if len(spending_transaction) == 0:
+    print("No matching transactions found in Address.")
+  elif len(spending_transaction) == 1:
+    print("Exact transaction found.")
+  elif len(spending_transaction) > 1:
+    print("Multiple possible transactions found.")
+
+
+def previous_spending_transaction(input):
+  # Tries to match up where the transaction came from
+  addr = input['address']
+  value = input['satoshis']
+  address = get_address(addr)
+  valid_transactions = filter_address(address["txs"], False)
+
+  spending_transaction = [tx for tx in valid_transactions # A list of transactions where:
+    if ( addr in [y['addr'] for y in tx['out']] # The address is an input
+    and value in [y['value'] for y in tx['out']])] # And the value matches
+
+  return spending_transaction[0]
+
+
+def reduce_addresses(transaction):
+  # Go over the addresses and combine the satoshis of a single address into one
+  newIns = []
+  newOuts = []
+  for input in transaction['inputs']:
+    if input in newIns:
+      newIns = combine_satoshis(newIns, input['address'], input['satoshis'])
+    else:
+      newIns.append(input)
+
+  for output in transaction['outputs']:
+    if output['address'] in [y['address'] for y in newOuts]:
+      newOuts = combine_satoshis(newOuts, output['address'], output['satoshis'])
+    else:
+      newOuts.append(output)
+
+  transaction['inputs'] = newIns
+  transaction['outputs'] = newOuts
+  return transaction
+
+
+def combine_satoshis(addresses, address, satoshis):
+  # Adds the sats together
+  for addr in addresses:
+    if addr['address'] == address:
+      addr['satoshis'] = addr['satoshis'] + satoshis
+      break
+
+  return addresses
 
 
 @app.route('/blockbuster/api/transaction/<transaction_id>')
 def get_transaction(transaction_id):
-  res = make_request(transaction_id)
-  return jsonify({'transaction': res})
+  root = get_root_trans(transaction_id)
+
+  # Expand to the inputs
+  for input in root['inputs']:
+    print('doing input')
+    time.sleep(10)
+    if input['address'] == 'Invalid Address':
+      print('invalid')
+    else:
+      input['parent'] = process_transaction(previous_spending_transaction(input))
+
+  # Expand to the outputs
+  for output in root['outputs']:
+    print('doing output')
+    time.sleep(10)
+    if output['address'] == 'Invalid Address':
+      print('invalid')
+    else:
+      output['child'] = process_transaction(next_spending_transaction(output))
+
+  res =  jsonify({'transaction': root})
+  return res
 
     
 if __name__ == "__main__":
